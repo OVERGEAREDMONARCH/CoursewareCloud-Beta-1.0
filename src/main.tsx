c39ae1ec44ce8a171cd4d153b46692fafc904d70
overgearedmonarch <oladimejiemmanuel543@gmail.com>
2025-07-13 18:13:05 +0100

    Beta-1.0

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/auth';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <App />
         <Analytics />
+        <SpeedInsights />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
