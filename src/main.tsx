61ff0992e4c4def334e2448d9cbf5010b71ffa40
overgearedmonarch <oladimejiemmanuel543@gmail.com>
2025-07-13 17:47:29 +0100

    Beta-1.0

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App'; // Renamed CoursewareCloud to App for consistency with 
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/auth';
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
